be905cc8cf878c0eb3429f9022ea52d299ff48cf
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-17 19:54:44 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+let theNames=["Guadalupe","Ollie","Aki"];
+for (let i=theNames[0];i<=theNames.length;i++);
+let myMessages=[`Thank you ${i}, for the wonderful gift!`];
+function writeCards(myMessages){
+    console.log(myMessages);
+}
+function countDown() {
+    let myNumber=10;
+    while (myNumber>=0){
+        console.log(myNumber--);
+    }
+    
+
+}
